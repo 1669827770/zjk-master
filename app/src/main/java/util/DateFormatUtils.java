405f7f7ffa648c5b62557caf31c93c8683c06cfd
1669827770 <1669827770@qq.com>
2017-07-25 10:56:38 +0800

    hahah

--- app/src/main/java/util/DateFormatUtils.java
+++ app/src/main/java/util/DateFormatUtils.java
@@ -22,6 +22,32 @@ import videodemo.R;
  */
 public class DateFormatUtils {
 
+    //http://blog.csdn.net/li419360214/article/details/49025189
+    public static String getStandardDate(String timeStr) {
+        String temp = "";
+        try {
+            long now = System.currentTimeMillis() / 1000;
+            long publish = Long.parseLong(timeStr);
+            long diff = now - publish;
+            long months = diff / (60 * 60 * 24*30);
+            long days = diff / (60 * 60 * 24);
+            long hours = (diff - days * (60 * 60 * 24)) / (60 * 60);
+            long minutes = (diff - days * (60 * 60 * 24) - hours * (60 * 60)) / 60;
+            if (months > 0) {
+                temp = months + "月前";
+            } else if (days > 0) {
+                temp = days + "天前";
+            } else if (hours > 0) {
+                temp = hours + "小时前";
+            } else {
+                temp = minutes + "分钟前";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return temp;
+    }
+
     // 一分钟的毫秒值
     public static final long ONE_MINUTE = 60 * 1000;
 
--- app/src/main/java/view/VideoTrimmerView.java
+++ app/src/main/java/view/VideoTrimmerView.java
@@ -4,7 +4,10 @@ package view;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
@@ -16,6 +19,7 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.VideoView;
 
 import java.util.ArrayList;
@@ -48,10 +52,39 @@ public class VideoTrimmerView extends FrameLayout {
     ArrayList<Bitmap> bitmaps;
     HomeAdapter homeAdapter;
 
+    /**
+     * 左右手柄按下的坐标
+     */
+    private int  lastLeftX;
+    private int  lastRightX;
+    /**
+     * 视频播放进度
+     */
+    private SeekBar sk_progressbar;
 
-    int  lastLeftX;
-    int  lastRightX;
+    /**
+     * 处理Handle的常量，根据视频播放的进度，更新SeekBar的滑动位置
+     */
+    private static final int MSG_UPDATE_SYSTEM_TIME = 0;
+    private static final int MSG_UPDATE_POSTITION = 1;
+    private static final int MSG_HIDE_CONTROLOR = 2;
 
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case MSG_UPDATE_SYSTEM_TIME:
+//                    startUpdateSystemTime();
+                    break;
+                case MSG_UPDATE_POSTITION:
+                    startUpdatePosition();
+                    break;
+                case MSG_HIDE_CONTROLOR:
+//                    hideControlor();
+                    break;
+            }
+        }
+    };
 
 
     public VideoTrimmerView(Context context) {
@@ -65,20 +98,28 @@ public class VideoTrimmerView extends FrameLayout {
     public VideoTrimmerView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(context);
+        initlistener();
     }
+
 
     private void init(Context context) {
 
         LayoutInflater.from(context).inflate(R.layout.video_trimmer_view, this, true);
-
         vv_trimvideo = findViewById(R.id.vv_trimvideo);
         rl_trimcontainer = findViewById(R.id.rl_trimcontainer);
         rl_trimpictureRecycleview = findViewById(R.id.rl_trimpictureRecycleview);
         iv_leftbar = findViewById(R.id.iv_leftbar);
         iv_rightbar = findViewById(R.id.iv_rightbar);
+        sk_progressbar = findViewById(R.id.sk_progressbar);
 
+        /**
+         * 存放缩略图的集合
+         */
         bitmaps = new ArrayList<>();
 
+        /**
+         * 缩略图的recycleview
+         */
         homeAdapter = new HomeAdapter();
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
@@ -86,9 +127,9 @@ public class VideoTrimmerView extends FrameLayout {
         rl_trimpictureRecycleview.setAdapter(homeAdapter);
 
 
-
-
-
+        /**
+         * 左手柄的滑动
+         */
         iv_leftbar.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -102,13 +143,23 @@ public class VideoTrimmerView extends FrameLayout {
                         break;
                     case MotionEvent.ACTION_MOVE:
                         int dx = (int) motionEvent.getRawX() - lastLeftX;
-
-                        int left = iv_leftbar.getLeft() + dx;
-
+                        int lBarleft = iv_leftbar.getLeft() + dx;
                         RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) iv_leftbar
                                 .getLayoutParams();
 
-                        layoutParams.leftMargin =left;
+//-------------------------------------------------------------------
+                        /**
+                         * 下面这几行代码用于控制两个手柄最短的距离的，其实没必要判断左手柄一定再右手柄的左边，只有有距离了
+                         * 就说明在左边
+                         */
+                        RelativeLayout.LayoutParams rightlayoutParams = (RelativeLayout.LayoutParams) iv_rightbar
+                                .getLayoutParams();
+                        int leftMargin = rightlayoutParams.leftMargin;
+
+                        if(leftMargin-lBarleft>120 && lBarleft>0){   //大于0是为了防止左手柄滑出界面
+                            layoutParams.leftMargin =lBarleft;
+                        }
+//-------------------------------------------------------------------
                         view.setLayoutParams(layoutParams);
                         lastLeftX = (int) motionEvent.getRawX();
                         break;
@@ -118,6 +169,9 @@ public class VideoTrimmerView extends FrameLayout {
             }
         });
 
+         /**
+          * 右手柄的滑动
+          */
         iv_rightbar.setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -131,14 +185,24 @@ public class VideoTrimmerView extends FrameLayout {
                         break;
                     case MotionEvent.ACTION_MOVE:
                         int dx = (int) motionEvent.getRawX() - lastRightX;
+                        int rBarleft = iv_rightbar.getLeft() + dx;
 
-                        int left = iv_rightbar.getLeft() + dx;
+                        RelativeLayout.LayoutParams rightlayoutParams = (RelativeLayout.LayoutParams) iv_rightbar.getLayoutParams();
+                        int screenWidth = ScreenUtils.getScreenWidth();
 
-                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) iv_rightbar
+//-------------------------------------------------------------------
+                        /**
+                         * 下面这几行代码用于控制两个手柄最短的距离的，其实没必要判断左手柄一定再右手柄的左边，只有有距离了
+                         * 就说明在左边
+                         */
+                        RelativeLayout.LayoutParams leftlayoutParams = (RelativeLayout.LayoutParams) iv_leftbar
                                 .getLayoutParams();
-
-                        layoutParams.leftMargin =left;
-                        view.setLayoutParams(layoutParams);
+                        int leftMargin = leftlayoutParams.leftMargin;
+                        if(rBarleft-leftMargin>120 && rBarleft<=screenWidth){ //小于等于屏幕宽度是为了防止右手柄滑出界面
+                            rightlayoutParams.leftMargin =rBarleft;
+                        }
+//-------------------------------------------------------------------
+                        view.setLayoutParams(rightlayoutParams);
                         lastRightX = (int) motionEvent.getRawX();
                         break;
                 }
@@ -147,6 +211,19 @@ public class VideoTrimmerView extends FrameLayout {
             }
         });
 
+    }
+
+    private void initlistener() {
+        sk_progressbar.setOnSeekBarChangeListener(new OnVideoSeekBarChangeListener());
+
+        /**
+         *  注册视频监听
+         */
+        /**
+         *  注册视频监听    1.准备，2完成
+         */
+        vv_trimvideo.setOnPreparedListener(new OnVideoPreparedListener());
+        vv_trimvideo.setOnCompletionListener(new OnVideoCompletionListener());
     }
 
 
@@ -184,7 +261,9 @@ public class VideoTrimmerView extends FrameLayout {
         Uri pathUri = Uri.parse(videoPath);
 
 
-        //重点来了就是剪切视频的工具类
+        /**
+         * 重点来了就是剪切视频的工具类
+         */
         TrimVideoUtil.backgroundShootVideoThumb(1,mContext, pathUri, new SingleCallback<ArrayList<Bitmap>, Integer>() {
             @Override
             public void onSingleCallback(final ArrayList<Bitmap> bitmap, final Integer interval) {
@@ -195,19 +274,18 @@ public class VideoTrimmerView extends FrameLayout {
                         bitmaps.addAll(bitmap);
                         homeAdapter.notifyDataSetChanged();
 
-
-
-
                     }
                 }, 0L);
 
             }
         });
 
-
-
     }
 
+    /**
+     * 播放视频
+     * @param videoPath
+     */
     public void placevideo(String videoPath) {
         //设置视频路径
         vv_trimvideo.setVideoURI(Uri.parse(videoPath));
@@ -282,6 +360,76 @@ public class VideoTrimmerView extends FrameLayout {
 //            return true;
 //
 //    }
+
+
+    /**
+     * 音量与视频播放进度的SeekBars的监听,即滑动SeekBar改变视频播放的进度与音量的大小
+     */
+    private class OnVideoSeekBarChangeListener implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        /** 当进度发生变化的时候被调用 */
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//            logE("OnVideoSeekBarChangeListener.onProgressChanged,progress="+progress+";fromUser="+fromUser);
+            // 不是用户发起的操作，则不处理，
+            if (!fromUser){
+                return;
+            }
+
+            switch (seekBar.getId()){
+                case R.id.sk_progressbar:
+                    vv_trimvideo.seekTo(progress);   //改变视频播放进度
+                    break;
+            }
+
+        }
+
+        @Override
+        /** 当手势按下的时候被调用 */
+        public void onStartTrackingTouch(SeekBar seekBar) {
+//            logE("OnVideoSeekBarChangeListener.onStartTrackingTouch,");
+//            mHandler.removeMessages(MSG_HIDE_CONTROLOR);
+        }
+
+        @Override
+        /** 当手指移开的时候被调用*/
+        public void onStopTrackingTouch(SeekBar seekBar) {
+//            logE("OnVideoSeekBarChangeListener.onStopTrackingTouch,");
+//            notifyHideControlor();
+        }
+    }
+
+    private class OnVideoPreparedListener implements MediaPlayer.OnPreparedListener {
+        @Override
+        public void onPrepared(MediaPlayer mp) {
+            // 资源准备完成，开始播放
+            vv_trimvideo.start();
+            // 更新播放进度
+            int duration = vv_trimvideo.getDuration();
+            sk_progressbar.setMax(duration);
+
+            startUpdatePosition();//获取视频播放进度，更新SeekBar
+        }
+    }
+
+    /** 获取视频播放进度，更新SeekBar*/
+    private void startUpdatePosition() {
+        int position = vv_trimvideo.getCurrentPosition();
+        sk_progressbar.setProgress(position);
+        // 发送延迟消息
+        mHandler.sendEmptyMessageDelayed(MSG_UPDATE_POSTITION, 500);
+    }
+
+
+
+    public class OnVideoCompletionListener implements MediaPlayer.OnCompletionListener {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            // 视频播放结束
+            // 由于系统bug，需要强制设置播放进度为视频总时长
+            sk_progressbar.setProgress(vv_trimvideo.getDuration());
+
+        }
+    }
 
 
     class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>
